19e85e2a04668a84a17db76be75a5cdc17f2d592
Tarsis Araujo <115889701+JahaTech@users.noreply.github.com>
2026-01-04 18:10:03 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.title("DashBoard")
+st.sidebar.markdown("# Porto Norte")
+st.sidebar.markdown("### Selecione a Subestação:")
+
+# Lateral Esqueda
+st.sidebar.text("Descarregamento:")
+st.sidebar.button("SE311KP-D")
+st.sidebar.button("SE311KP-E")
+st.sidebar.button("SE311KP-F")
+# --------
+st.sidebar.text("Embarque:")
+st.sidebar.button("SE3xxKP-x1")
+st.sidebar.button("SE3xxKP-x2")
+st.sidebar.button("SE3xxKP-x3")
+st.sidebar.button("SE3xxKP-x4")
+st.sidebar.button("SE3xxKP-x5")
+st.sidebar.button("SE3xxKP-x6")
+st.sidebar.button("SE3xxKP-x7")
+
+
+
+
+def exibir():
