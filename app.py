76c9f8c0bbb846e2ec2c89ebc278a204e2e1bbd5
Tarsis Araujo <115889701+JahaTech@users.noreply.github.com>
2026-01-04 22:43:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def exibir(topic):
 
 # -- Titulo do Dashboard
 st.title("DashBoard - :rainbow[VALE]")
-#st.balloons()
+st.balloons()
 st.sidebar.markdown("# Porto Norte")
 st.sidebar.markdown("### Selecione a Subestação:")
 
@@ -24,13 +24,11 @@ if st.sidebar.button("SE311KP-D"):
 if st.sidebar.button("SE311KP-E"):
     pass
 if st.sidebar.button("SE311KP-F"):
-    VV07 = exibir('enterprise/SE11F/VV07/temp')
-    VV08 = exibir('enterprise/SE11F/VV08/temp')
-    print(VV07, VV08)
+    VV07 = 21
+    VV08 = 19
     st.text("Temperatura VV07: "+str(VV07)+"ºC")
     st.text("Temperatura VV08: "+str(VV08)+"ºC")
     st.bar_chart({"VV07": VV07, "VV08": VV08},color="#ff2c2c")
-
 
 # --------
 st.sidebar.text("Embarque:")
